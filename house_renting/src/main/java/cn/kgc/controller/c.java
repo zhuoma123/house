39e0fd8a72365c4022ce14e392316cf2dd9b6771
zhuoma <924646875@qq.com>
2020-01-05 14:44:18 +0800

    房屋

---
+++ house_renting/src/main/java/cn/kgc/controller/c.java
@@ -0,0 +1,13 @@
+package cn.kgc.controller;
+
+/**
+ * @Version 1.0
+ * @Author:dama zhuo
+ * @Date:2020/1/5 14:35
+ * @Content:
+ */
+public class c {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
--- house_renting/src/main/java/cn/kgc/controller/d.java
+++ house_renting/src/main/java/cn/kgc/controller/d.java
@@ -3,8 +3,8 @@ package cn.kgc.controller;
 /**
  * @Version 1.0
  * @Author:dama zhuo
- * @Date:2020/1/3 17:16
+ * @Date:2020/1/5 14:36
  * @Content:
  */
-public class a {
+public class d {
 }
